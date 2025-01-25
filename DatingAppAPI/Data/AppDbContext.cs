b0c1f4410ce9bfa1bfd46ed91b406438a20f520e
Naveen7105official <snk2022it@gmail.com>
2025-01-26 00:51:56 +0530

    Initial

---
+++ DatingAppAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DatingAppAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<AppUser> User { get; set; }
+    }
+}
